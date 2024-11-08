0bdc45daad580392bb03be32cbc1fcca7932f17b
Janne Kärki <janne.karki@fmi.fi>
2024-11-08 16:05:11 +0200

    Exercise 1.05 Done

--- project/todo-app.py
+++ project/todo-app.py
@@ -5,8 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Server started in port " + os.getenv("PORT", "5000")
+    return """
+            <html>
+                <head>
+                    <title>Todo-App-Server</title>
+                </head>
+                <body>
+                    <h1> Hello </h1>
+                </body>
+            </html>
+            """
+
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
