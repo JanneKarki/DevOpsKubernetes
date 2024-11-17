545fcbe209e66210f912db68d0b8fc197695d012
Janne Kärki <janne.karki@fmi.fi>
2024-11-17 13:35:01 +0200

    Exercise 1.11 Done

--- exercises/1/log-output-app/log-reader.py
+++ exercises/1/log-output-app/log-reader.py
@@ -3,22 +3,25 @@ import hashlib
 import uuid
 
 app = Flask(__name__)
-file_path = "/usr/src/app/files/timestamp.txt"
+timestamp_path = "/usr/src/app/files/timestamp.txt"
+pingpong_path = "/usr/src/app/files/pingpong.txt"
 
 random_string = str(uuid.uuid4())
 
 @app.route('/', methods=['GET'])
 def get_status():
     try:
-        with open(file_path, "r") as file:
-            timestamp = file.read()
+        with open(timestamp_path, "r") as timestamp_file:
+            timestamp = timestamp_file.read()
+        with open(pingpong_path, "r") as pingpong_file:
+            ping_count = pingpong_file.read()
+
         hash_value = hashlib.sha256(random_string.encode()).hexdigest()
-        return jsonify({
-            "timestamp": timestamp,
-            "hash": hash_value
-        })
+        return f"{timestamp}: {hash_value}. \nPing / Pongs : {ping_count}"
+
     except FileNotFoundError:
         return jsonify({"error": "No timestamp file found"}), 404
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
--- exercises/1/pingpong-app/pingpong.py
+++ exercises/1/pingpong-app/pingpong.py
@@ -2,13 +2,15 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
-
+file_path = "/usr/src/app/files/pingpong.txt"
 counter = 0
 
 @app.route('/pingpong', methods=['GET'])
 def ping_pong():
     global counter
-    response = f"pong {counter}\n"
+    response = f"{counter}\n"
+    with open(file_path, "w") as file:
+        file.write(response)
     counter += 1
     return response
 
