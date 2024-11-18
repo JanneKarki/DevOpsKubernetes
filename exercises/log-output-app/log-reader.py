ec2d1241095087d2bb41cbb5d4119b4c33155315
Janne Kärki <janne.karki@fmi.fi>
2024-11-18 10:26:52 +0200

    Exercise 2.01: Connecting pods Done

--- exercises/log-output-app/log-reader.py
+++ exercises/log-output-app/log-reader.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify
 import hashlib
 import uuid
+import requests
 
 app = Flask(__name__)
 timestamp_path = "/usr/src/app/files/timestamp.txt"
-pingpong_path = "/usr/src/app/files/pingpong.txt"
+pingpong_service_url = "http://pingpong-svc:8080/pingpong"  
 
 random_string = str(uuid.uuid4())
 
@@ -13,14 +14,18 @@ def get_status():
     try:
         with open(timestamp_path, "r") as timestamp_file:
             timestamp = timestamp_file.read()
-        with open(pingpong_path, "r") as pingpong_file:
-            ping_count = pingpong_file.read()
 
+        response = requests.get(pingpong_service_url)
+        response.raise_for_status()
+        ping_count = response.text.strip()
+        
         hash_value = hashlib.sha256(random_string.encode()).hexdigest()
         return f"{timestamp}: {hash_value}. \nPing / Pongs : {ping_count}"
 
     except FileNotFoundError:
         return jsonify({"error": "No timestamp file found"}), 404
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": f"Error connecting to pingpong service: {e}"}), 500
 
 
 if __name__ == '__main__':
