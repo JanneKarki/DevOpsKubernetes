ce6c298285ffd89906ab4bd8df4eaa632cacc64a
Janne Kärki <janne.karki@fmi.fi>
2024-11-19 11:17:30 +0200

    Exercise 2.06: Documentation and ConfigMaps Done

--- exercises/log-output-app/log-reader.py
+++ exercises/log-output-app/log-reader.py
@@ -2,10 +2,12 @@ from flask import Flask, jsonify
 import hashlib
 import uuid
 import requests
+import os
 
 app = Flask(__name__)
 timestamp_path = "/usr/src/app/files/timestamp.txt"
 pingpong_service_url = "http://pingpong-svc:8080/pingpong"  
+information = "/usr/src/app/config/information.txt"
 
 random_string = str(uuid.uuid4())
 
@@ -18,9 +20,21 @@ def get_status():
         response = requests.get(pingpong_service_url)
         response.raise_for_status()
         ping_count = response.text.strip()
+
+
+        with open(information, "r") as information_file:
+            file_content = information_file.read().strip()
         
+        message = os.getenv("MESSAGE", "MESSAGE not set")
+
         hash_value = hashlib.sha256(random_string.encode()).hexdigest()
-        return f"{timestamp}: {hash_value}. \nPing / Pongs : {ping_count}"
+        
+        return f"""
+                file content: {file_content}
+                env variable: MESSAGE={message}
+                {timestamp}: {hash_value}.
+                Ping / Pongs: {ping_count}
+                """
 
     except FileNotFoundError:
         return jsonify({"error": "No timestamp file found"}), 404
