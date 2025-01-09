7f148de860ae18fbf1a716c5b71fb68c7d32be04
Janne Kärki <janne.karki@fmi.fi>
2025-01-09 14:58:57 +0200

    Exercise 2.10: Project v1.3 Done

--- project/todo-app.py
+++ project/todo-app.py
@@ -2,17 +2,19 @@ from flask import Flask, send_file, request
 import os
 import requests
 from datetime import datetime, timedelta
+import logging
 
 app = Flask(__name__)
 image_path = "/usr/src/app/files/hourly_image.jpg"
 update_interval = timedelta(hours=1)
 backend_service_url = os.getenv("BACKEND_SERVICE_URL")
-
+logging.basicConfig(level=logging.INFO)
 
 def download_image():
     response = requests.get("https://picsum.photos/1200")
     with open(image_path, "wb") as file:
         file.write(response.content)
+    logging.info("Image downloaded successfully")
 
 def check_image_update():
     if os.path.exists(image_path):
@@ -28,6 +30,11 @@ def home():
         new_todo = request.form.get('todo')
         if new_todo:
             response = requests.post(backend_service_url, json={"todo": new_todo})
+            if response.status_code == 201:
+                logging.info("Todo successfully sent to backend: %s", new_todo)
+            else:
+                logging.error("Failed to create todo: %s | Backend response: %s", new_todo, response.json())
+                
     response = requests.get(backend_service_url)
     todos = response.json()
     
@@ -43,7 +50,7 @@ def home():
                     <h1>Todo App</h1>
                     <img src="/todo/image" alt="Random Image">
                     <form method="POST">
-                        <input type="text" name="todo" maxlength="140">
+                        <input type="text" name="todo">
                         <button type="submit">Create TODO</button>
                     </form>
                     <ul>
--- project/todo_backend/todo-backend.py
+++ project/todo_backend/todo-backend.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
 import psycopg2
 import os
+import logging
 
 app = Flask(__name__)
+
+logging.basicConfig(level=logging.INFO)
 
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
@@ -38,19 +41,26 @@ def get_todos():
         cur.execute("SELECT task FROM todos;")
         todos = [row[0] for row in cur.fetchall()]
     conn.close()
+    logging.info("GET: Retrieved todos")
     return jsonify(todos)
 
 @app.route('/', methods=['POST'])
 def create_todo():
     new_todo = request.json.get('todo')
     if new_todo:
+        if len(new_todo) > 140:
+            logging.warning("Todo rejected: exceeds 140 characters %s", new_todo)
+            return jsonify({"error": "Todo exceeds 140 characters"}), 400 
         conn = get_db_connection()
         with conn.cursor() as cur:
             cur.execute("INSERT INTO todos (task) VALUES (%s);", (new_todo,))
             conn.commit()
         conn.close()
+        logging.info("Todo added successfully: %s", new_todo)
         return jsonify({"message": "Todo added successfully"}), 201
+    logging.error("Invalid todo data received: %s", new_todo)
     return jsonify({"error": "Invalid todo"}), 400
+
 
 if __name__ == '__main__':
     initialize_database()
